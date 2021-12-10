1f68f9f7c1d51c5b0c2a70d7724a13ccd5a2c4cb
achmad-mustafa777 <achmadmustafa777@gmail.com>
2021-12-10 19:58:54 +0800

    MVC Assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,7 @@
-<h1>About me</h1>
-<p>Hallo nama saya <?= $data['nama']; ?>, pekerjaan saya adalah <?= $data['profesi']; ?> dan saya berusia <?= $data['umur']; ?> tahun</p>
+<div class="container">
+
+  <h1>About me</h1>
+  <img src="<?= BASEURL; ?>/img/heacker.jpg" alt="Topa" width="200" class="rounded-circle shadow">
+  <p>Hallo nama saya <?= $data['nama']; ?>, pekerjaan saya adalah <?= $data['profesi']; ?> dan saya berusia <?= $data['umur']; ?> tahun</p>
+
+</div>
--- public/index.php
+++ public/index.php
@@ -5,58 +5,45 @@ require_once '../app/init.php';
 $app = new App();
 
 /*
-! MVC Views
+! MVC Assets
 
 todo: Tujuan pembelajaran
 
--> mengganti isi dari tiap-tiap controller dengan memanggil view / interface yang-
-	 terdapat didalam folder views
-	 
+-> mengelola assets yang dimiliki yang ada di dalam folder Public
+-> menerapkan framework boostrep css dan menampilkan gambar
+
 todo: Yang akan dilakukan
 
--> Memanggil method view di folder controller class Home dan About, dimana method view ter-
-	 dapat di folder core di class Controller
+-> Download bootsrepnya yang sudah dicompile di websitenya
+-> pindahkan filenya ke public
+-> menginclude / memanggil file bootsrtap di folder views
+
+	-> menambahkan link yang diarahkan ke boostrap css di folder templates/header.php
+	-> menambahkan link js di folder templates/footer.php
+		 -> menambahkan link jQuery, poopernya dan link localnya (absolut url)
+		 
+-> buat file baru di folder core dengan nama file Constants.php yang bertujuan-
+	 untuk menyimpan constanta-constanta yang kita butuhkan misalnya url absolut
 	 
-	 -> isi methodnya berupa alamat/link yang menghubungkan ke folder views yang-
-	 		akan kita coba akses
-	 -> buat dulu file di folder views/home dengan nama file index.php yang isinya-
-	 		berupa html yang nantinya akan menjadi viewnya yang akan di access lewat-
-	 		controller
+	 -> buat constantanya dengan nama BASEURL
+	 -> lakukan require di file init.php
+	 -> ubah link url absolutnya yang ada di file header.php dan footer.php dengan-
+	 		constanta yang baru saja kita buat
 	 		
--> Buat method yang akan mengarahkan ke folder views di folder core di dalam-
-	 class Controller dengna 2 parameter. Paremeter pertama adalah link ke views,
-	 parameter kedua adalah berupa data yang akan dikirimkan ke view berupa array.
-	 Sementara dibuat data defaultnya berupa array kosong.
+-> kita modif tampilan sesuai dengan keinginan kita menggunakan boostrap, misal-
+	 nya kita ingin menambahkan navbarnya
 	 
-	 -> isi methodnya berupa memanggil viewnya dengan menggunakan fungsi 'require_once'
-	 -> jadikan class Home yang ada di folder controller sebagai child class dari-
-	 		class Controller dengan menambahkan 'extends' 
-	 		
--> Beralih ke controller About yang memiliki 2 method, yang nantinya akan mem-
-	 buat 2 buah view, yaitu method index dan method page
+	 -> ke situs bootsrap cari bagian navbarnya
+	 -> copy code cssnya
+	 -> paste codenya di bagaian file header.php
+	 -> jalankan linknya di navbar
 	 
-	 -> method pertama akan memanggil method view yang akan diarahkan ke folder-
-	 		about/index
-	 -> method kedua akan memanggil method view yang diarahkan ke folder about/pages
-	 -> buat folder about di folder views dan file index dan page
-	 -> jadikan class About yang di folder controller menjadi class child dari-
-	 		class Controller
-	 -> Sekarang mengirimkan data dari class About ke folder views menggunakan method-
-	 		view
-	 -> buat variabel array di method index yang isinya adalah parameter dari method-
-	 		index itu sendiri yang akan menjadi parameter ke dua dari method view
-	 		
--> Buat template html dengan tujuan tidak melakukan pekerjaan yang berulang-ulang-
-	 / menerapkan DRY agar lebih efidien dan code menjadi lebih elegant
+-> beralih ke folder views/home/index.php untuk menambahkan tampilannya dengan -
+	 jumbotrons
 	 
-	 -> buat folder dengan nama templates di dalam folder views
-	 -> buat 2 file di dalam folder template yaitu file header.php & file footer.php-
-	 		yang isinya adalah header html untuk file header.php & footer html untuk-
-	 		file footer.php
-	 -> lalu kita rangkai link menuju templates di folder controller , dan terapkan-
-	 		ke seluruh class di dalam folder controller 
-	 
-	 
-		 	
+	 -> ke situs bootsrap cari bagian jumbotrons
+	 -> copy code htmlnya
+	 -> paste codenya di file index.php dengan tujuan sebagai mempercantik tampilan-
+	 		mengguankan bootsrap framework
 		
  */
